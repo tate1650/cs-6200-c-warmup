58cdfd84f8177864c9a6b0da01050a10e86c6f12
Tate Mauzy <tatenm2@gmail.com>
2025-01-13 03:28:59 +0000

    Read command line args

--- sum-input.c
+++ sum-input.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+// TOOD: Check tests-to-write.todo for test cases I should check for in munit
+
 int main(int argc, char *argv[])
 {
-    // Your code here
+  if (argc != 3) {
+    printf("Usage: %s INPUT_PATH OUTPUT_PATH\n", argv[0]);
+    exit(1);
+  }
+
+  char *input_file_name = argv[1];
+  char *output_file_name = argv[2];
+
+  FILE *input_file_ptr;
+  FILE *output_file_ptr;
+
+  if (input_file_ptr = fopen(input_file_name, "r") == NULL) {
+    printf("Error: File at path %s not found", input_file_name);
+    exit(1);
+  } else if (output_file_ptr = fopen(output_file_name, "w") == NULL) {
+    printf("Error: File at path %s not found", output_file_name);
+    exit(1);
+  }
+
+  // TODO: Read input file
+
+  // TODO: convert input to ints and create sums
+
+  // TODO: write sums to output file
+
+  fclose(input_file_ptr);
+  fclose(output_file_ptr);
 }
